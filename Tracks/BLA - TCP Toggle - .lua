819d93c3e5a90e36dbd9de7541a2be66a9be29d5
Boomotang <brianlangeman@hotmail.com>
2023-05-12 15:24:18 -0400

    TCP Toggle - Start

---
+++ Tracks/BLA - TCP Toggle - .lua
@@ -0,0 +1,54 @@
+--------------------============--------------------
+----------<<<<<<<<<< TCP Toggle >>>>>>>>>>----------
+--------------------============--------------------
+
+---------------------------------------
+----- SET THE FOLLOWING VARIABLES -----
+---------------------------------------
+Group1 = 11111          --  << COMMAND ID
+TrackName1 = "      "     --  %d = if any digit is found
+TrackName2 = "      "
+TrackName3 = "      "
+
+
+tbTracks = {}     -- MediaTracks
+tbNames = {}      -- Names of MediaTracks
+tbNewTracks = {}  -- new selection of MediaTracks from specified Names
+
+reaper.Main_OnCommand(Group1, 0)  -- select group
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_SELCHILDREN2"), 0)
+
+trCount = reaper.CountSelectedTracks(0)
+
+-- put selected tracks into TABLE: tbTracks
+for i=0, trCount do
+  selTrack = reaper.GetSelectedTrack(0, i)
+  table.insert(tbTracks, selTrack)
+end
+
+-- get track names and put them in new TABLE: tbNames
+for i=1, #tbTracks do
+  _, selName = reaper.GetTrackName(tbTracks[i])
+  table.insert(tbNames, selName)
+end
+
+-- check names and specify ones to put in new TABLE:tbNewTracks from the TABLE:tbTracks
+for i=1, #tbNames do
+  if
+    string.find(tbNames[i], TrackName1) or
+    string.find(tbNames[i], TrackName2) or
+    string.find(tbNames[i], TrackName3) then
+      table.insert(tbNewTracks, tbTracks[i])
+  end
+end
+
+reaper.Main_OnCommand(40297, 0)  -- unselect all tracks
+
+-- select all tracks from TABLE: tbNewTracks
+for i=1, #tbNewTracks do
+  reaper.SetTrackSelected(tbNewTracks[i], true)
+end
+
+reaper.Main_OnCommand(40853, 0)  -- Toggle TCP Visibility
+
+reaper.Main_OnCommand(40297, 0)  -- unselect all tracks
