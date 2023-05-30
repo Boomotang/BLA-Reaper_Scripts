1c82c50b91c3a6968952b53dbbe2a2a4a178fbaf
Boomotang <brianlangeman@hotmail.com>
2023-05-30 11:34:03 -0400

    Scroll & Cycle - Finished

---
+++ Tracks/BLA - Scroll & Cycle - BASS.lua
@@ -0,0 +1,17 @@
+--------------------============================--------------------
+----------<<<<<<<<<< Scroll & Cycle to 2 Tracks >>>>>>>>>>----------
+--------------------============================--------------------
+
+---------------------------------------
+----- SET THE FOLLOWING VARIABLES -----
+---------------------------------------
+Group1 = 42255          --  << COMMAND ID
+
+-- Get the command_id-number Reaper uses internally for Scroll Mixer action.
+scrollMixer = reaper.NamedCommandLookup("_RS3362313dca57362b5f65ccc16fa19c12a2d6487e") -- <<<<<<<<<<<<<<< CHECK
+
+
+reaper.Main_OnCommand(Group1,0) -- select Group 1
+  
+reaper.Main_OnCommand(scrollMixer,0)
+reaper.Main_OnCommand(40913, 0)       -- vertical scroll in TCP
