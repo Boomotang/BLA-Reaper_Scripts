436ebf0a920545f9301386d4253591f95c03b10e
Boomotang <brianlangeman@hotmail.com>
2023-08-11 16:10:23 -0400

    MISC

--- TESTS/BLA - TESTING.lua
+++ TESTS/BLA - TESTING.lua
@@ -1,33 +1,8 @@
--- Only show tracks with automation envelopes.
+targetDB = -8
+targetSet = 10^(targetDB/20)
 
-tbTracks = {}     -- MediaTracks
-tbNewTracks = {}  -- new selection of MediaTracks from specified Names
+item = reaper.GetSelectedMediaItem(0, 0)
+take = reaper.GetActiveTake(item)
+reaper.SetMediaItemTakeInfo_Value(take, "D_VOL", targetSet)
 
-
-trCount = reaper.CountTracks(0)
-
-reaper.Main_OnCommand(40296, 0)
-
--- put selected tracks into TABLE: tbTracks
-for i=0, trCount do
-  selTrack = reaper.GetSelectedTrack(0, i)
-  table.insert(tbTracks, selTrack)
-end
-
-for i=1, #tbTracks do
-  EnvCount = reaper.CountTrackEnvelopes(tbTracks[i])
-  if EnvCount > 0 then
-    table.insert(tbNewTracks, tbTracks[i])
-  end
-end
-
-reaper.Main_OnCommand(40297, 0)  -- unselect all tracks
-
-
-
--- select all tracks from TABLE: tbNewTracks
-for i=1, #tbNewTracks do
-  reaper.SetTrackSelected(tbNewTracks[i], true)
-end
-
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWSTL_SHOWTCPEX"), 0)
+reaper.UpdateArrange()
